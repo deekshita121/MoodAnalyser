c258d99c4956f5f666e58b75b698fe1b46c65d48
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-06 22:14:26 +0530

    [deekshita] Initial Commit

---
+++ MoodAnalyser/src/main/java/com/capg/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,15 @@
+package com.capg.moodanalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class MoodAnalyser 
+{
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to Mood Analyser");
+    }
+}
